29a46cb9da3a185ee061ea25f27cbeadcd1c1e37
ritiksharmarj <theonlyritik@gmail.com>
2025-09-03 19:39:03 +0530

    fix: wxt version issue

--- entrypoints/popup/App.tsx
+++ entrypoints/popup/App.tsx
@@ -16,14 +16,12 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>WXT + React</h1>
+      <h1>Ritik is back</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+  
       </div>
       <p className="read-the-docs">
         Click on the WXT and React logos to learn more
