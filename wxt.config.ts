ec358fa9035530b171f304a66fab09dc452119f8
ritiksharmarj <theonlyritik@gmail.com>
2025-09-04 18:06:45 +0530

    feat: initialize popup app structure and styles

--- wxt.config.ts
+++ wxt.config.ts
@@ -2,5 +2,8 @@ import { defineConfig } from "wxt";
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
+  srcDir: "src",
+  outDir: "dist",
+  entrypointsDir: "app",
   modules: ["@wxt-dev/module-react"],
 });
